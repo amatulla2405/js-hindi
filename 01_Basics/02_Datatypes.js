4ec503f9f028b0316f86cbfa6a01c0c66dd77251
amatulla2405 <138687735+amatulla2405@users.noreply.github.com>
2024-01-29 12:59:44 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,32 @@
+"use strict"  //treat all JS code as newer version
+
+//alert(3+3)--->will not work as we are using nodejs, not browser
+
+console.log(2+2)
+console.log("Amatulla")
+
+let name="Amatulla"   //string
+let age=18            //integer
+let isLoggedIn=false  //boolean
+let state 
+//Some Primitive Datatypes
+/* 
+integer
+string
+boolean
+number
+bigint
+null =>standalone value
+undefined
+symbol
+*/
+
+//Non-primitive Datatypes
+//object
+
+
+console.log(typeof("Amatulla"))
+console.log(typeof(undefined))
+console.log(typeof(null))
+
+
