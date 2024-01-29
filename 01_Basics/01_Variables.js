86531e86d6fab1fdfc2ac4f2968a6f078aeab4fa
amatulla2405 <138687735+amatulla2405@users.noreply.github.com>
2024-01-29 10:35:45 +0000

    declare variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,23 @@
+const accountId=112245
+let accountEmail="amatulla@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState
+
+//accountId=13542---->Not allowed
+//console.log(accountId)
+
+accountEmail="am@231"
+//console.log(accountEmail)
+
+accountPassword="4321"
+//console.log(accountPassword)
+
+accountCity="Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var 
+because of issues in block scope and functional scope
+*/
